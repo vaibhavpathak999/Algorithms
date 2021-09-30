140a40443e2bca5bc1891caba16eda5085d47859
Raghav Gupta <66035668+raghavguptaa@users.noreply.github.com>
2021-10-01 03:34:30 +0530

    Linked List Traversal using Recursion.
    Traversal means “visiting” or examining each node of the list. We start from the beginning and visit one node at a time until the end of the list (until the ‘next’ is NULL). As discussed in the previous post, we need the first element (head) to reach to any element of the list. So, we will do the traversal using the ‘head’ and print its element and then proceed to the next element of the list.
    
    Thus, the steps for the traversal of the linked list are:
    
    Check if the element is not NULL.
    If it is not, then print its ‘data’.
    Change the element to the element stored in the ‘next’.

---
+++ LinkedList/linked-list_traversal.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+class linked_list
+{
+private:
+    node *head, *tail;
+
+public:
+    linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void add_node(int n)
+    {
+        node *tmp = new node;
+        tmp->data = n;
+        tmp->next = NULL;
+
+        if (head == NULL)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else
+        {
+            tail->next = tmp;
+            tail = tail->next;
+        }
+    }
+
+    node *gethead()
+    {
+        return head;
+    }
+
+    void display(node *head)
+    {
+        if (head == NULL)
+        {
+            cout << "NULL" << endl;
+        }
+        else
+        {
+            cout << head->data << endl;
+            display(head->next);
+        }
+    }
+};
+
+int main()
+{
+    linked_list a;
+    a.add_node(1);
+    a.add_node(2);
+    a.display(a.gethead());
+    return 0;
+}
